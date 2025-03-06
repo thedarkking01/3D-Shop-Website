dad28610dd44853145e21348a3d826c036e416ba
thedarkking01 <sourabhsinghbais52@gmail.com>
2025-03-06 18:45:53 +0530

    update code

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,12 +1,13 @@
 import CustomButton from "./CustomButton";
 import AIPicker from "./AIPicker";
-import FilePicker from "./FilePicker";
 import ColorPicker from "./ColorPicker";
+import FilePicker from "./FilePicker";
 import Tab from "./Tab";
-export {
-    CustomButton,
-    AIPicker,
-    ColorPicker,
-    FilePicker,
-    Tab,
+
+export { 
+  CustomButton,
+  AIPicker,
+  ColorPicker,
+  FilePicker,
+  Tab,
 };
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,21 +1,3 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
-// import { defineConfig } from 'vite';
-// import tailwindcss from '@tailwindcss/vite';
-
-// export default defineConfig({
-//   plugins: [
-//     tailwindcss(),
-//   ],
-// });
-
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
